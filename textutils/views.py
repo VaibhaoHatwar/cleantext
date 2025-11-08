3a1eb9f3f3555ef102fa1d97b9ab500781576343
Vaibhao Hatwar <vaibhaohatwar.works@gmail.com>
2025-11-08 21:00:13 +0530

    register textutils app and add URL routing setup

--- textutils/views.py
+++ textutils/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>Welcome to 🧹CleanText</h1><p>This is the home page.</p><a href='about'>About Page</a>")
+
+def about(request):
+    return HttpResponse("<h2>About 🧹CleanText</h2><p>This app helps clean your text easily!</p><a href='/'>Back to Home Page</a>")
